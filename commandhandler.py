0bc3eb205fcd9bc0c88e8e06ffb7781fc3bd476e
sri-vastav-netizen <sivi20@student.bth.se>
2021-03-17 16:38:51 +0100

    Rechange commands array

--- commandhandler.py
+++ commandhandler.py
@@ -37,14 +37,22 @@ class CommandHandler():
         """Returns commands description that can be used by the user to interact with file server.
         """
 
-        commands = ["register : To register as a new user ,command:register <username> <password> \n",
-                 "login : To login (only works if you already registered), command:login <username> <password>,Note:password should be in integer\n",
-                 "quit : To logout, command:quit\n",
-                 "change_folder : To change the current path, command:change_folder <name>\n",
-                 "list : Lists all files in the current path, command:list\n",
-                 "read_file : To read content from the file, command:read_file <name>\n",
-                 "write_file : To write content into the file, command:write_file <name> <content>\n",
-                 "create_folder : To create new folder, command:create_folder <name>\n"
+        commands = ["""register : To register as a new user ,
+                    command:register <username> <password> \n""",
+                    """login : To login , 
+                    command:login <username> <password>""",
+                    """quit : To logout, 
+                    command:quit\n""",
+                    """change_folder : To change the current path, 
+                    command:change_folder <name>\n""",
+                    """list : Lists all files in the current path, 
+                    command:list\n""",
+                    """read_file : To read content from the file, 
+                    command:read_file <name>\n""",
+                    """write_file : To write content into the file, 
+                    command:write_file <name> <content>\n""",
+                    """create_folder : To create new folder, 
+                    command:create_folder <name>\n"""
                 ]
 
         return "".join(commands)
--- tests.py
+++ tests.py
@@ -11,19 +11,28 @@ from commandhandler import CommandHandler
 
 
 class TestClient(unittest.TestCase):
-    
+    """[summary]
+    """
     def test_commands_output(self):
         """This test deals with testing whether correct description of 
         commands is returned by the server to client.
         """
-        commands = ["register : To register as a new user ,command:register <username> <password> \n",
-                 "login : To login (only works if you already registered), command:login <username> <password>,Note:password should be in integer\n",
-                 "quit : To logout, command:quit\n",
-                 "change_folder : To change the current path, command:change_folder <name>\n",
-                 "list : Lists all files in the current path, command:list\n",
-                 "read_file : To read content from the file, command:read_file <name>\n",
-                 "write_file : To write content into the file, command:write_file <name> <content>\n",
-                 "create_folder : To create new folder, command:create_folder <name>\n"
+        commands = ["""register : To register as a new user ,
+                    command:register <username> <password> \n""",
+                    """login : To login , 
+                    command:login <username> <password>""",
+                    """quit : To logout, 
+                    command:quit\n""",
+                    """change_folder : To change the current path, 
+                    command:change_folder <name>\n""",
+                    """list : Lists all files in the current path, 
+                    command:list\n""",
+                    """read_file : To read content from the file, 
+                    command:read_file <name>\n""",
+                    """write_file : To write content into the file, 
+                    command:write_file <name> <content>\n""",
+                    """create_folder : To create new folder, 
+                    command:create_folder <name>\n"""
                 ]
         expected = "".join(commands)
 
